1a3ca39df01ecc0577a21c4e532e5377519ca3ac
Wilmer Arambula <terabytesoftw@gmail.com>
2024-06-28 07:46:33 -0400

    Fix config tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -19,7 +19,7 @@ $composerAutoload = __DIR__ . '/../vendor/autoload.php';
 if (is_file($composerAutoload)) {
     require_once $composerAutoload;
 }
-require_once __DIR__ . '/../framework/Yii.php';
+require_once __DIR__ . '/../src/Yii.php';
 
 Yii::setAlias('@yiiunit', __DIR__);
 
