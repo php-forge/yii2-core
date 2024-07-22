07bc5038bf1d9926ce9412714e9565152cb10ebc
Wilmer Arambula <terabytesoftw@gmail.com>
2024-07-22 10:10:59 -0400

    Add null check for user in AccessRule allows method.

--- src/filters/AccessRule.php
+++ src/filters/AccessRule.php
@@ -169,6 +169,10 @@ class AccessRule extends Component
      */
     public function allows($action, $user, $request)
     {
+        if ($user === null) {
+            return null;
+        }
+
         if (
             $this->matchAction($action)
             && $this->matchRole($user)
