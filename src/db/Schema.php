d8c58736e3e358d0c8c4176a3b7881e93ac7019b
Wilmer Arambula <terabytesoftw@gmail.com>
2024-08-07 07:15:57 -0400

    Update table and column quote characters in database schema classes.

--- src/db/Schema.php
+++ src/db/Schema.php
@@ -94,13 +94,13 @@ abstract class Schema extends BaseObject
      * An array of 2 characters can be used in case starting and ending characters are different.
      * @since 2.0.14
      */
-    protected $tableQuoteCharacter = "'";
+    protected array|string $tableQuoteCharacter = "'";
     /**
      * @var string|string[] character used to quote column names.
      * An array of 2 characters can be used in case starting and ending characters are different.
      * @since 2.0.14
      */
-    protected $columnQuoteCharacter = '"';
+    protected array|string $columnQuoteCharacter = '"';
 
     /**
      * @var array list of ALL schema names in the database, except system schemas
