76a72e0657cf2ca8d22a4592798ce687a074ef29
Wilmer Arambula <terabytesoftw@gmail.com>
2024-08-11 10:49:24 -0400

    chore: Update MSSQL configuration to use stronger password.

--- tests/support/MssqlConnection.php
+++ tests/support/MssqlConnection.php
@@ -8,6 +8,6 @@ final class MssqlConnection extends AbstractConnection
 {
     public static string $dsn = 'sqlsrv:Server=127.0.0.1,1433;Database=yiitest';
     public static string $fixture = 'mssql.sql';
-    public static string $password = 'YourStrong!Passw0rd';
+    public static string $password = 'Password12!';
     public static string $username = 'SA';
 }
