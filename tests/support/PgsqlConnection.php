239441375b4779ebf5b42b7bfc902a8367fa37ed
Wilmer Arambula <terabytesoftw@gmail.com>
2024-08-16 10:41:20 -0400

    chore: Update fixture file name in PgsqlConnection.php.

--- tests/support/PgsqlConnection.php
+++ tests/support/PgsqlConnection.php
@@ -7,7 +7,7 @@ namespace yiiunit\support;
 final class PgsqlConnection extends AbstractConnection
 {
     public static string $dsn = 'pgsql:host=localhost;dbname=yiitest;port=5432;';
-    public static string $fixture = 'pgsql.sql';
+    public static string $fixture = 'postgres.sql';
     public static string $password = 'root';
     public static string $username = 'root';
 }
