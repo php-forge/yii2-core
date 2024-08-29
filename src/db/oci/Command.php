f832dbc1c2b048e6453212fd9f74c0bbbe67683f
Wilmer Arambula <terabytesoftw@gmail.com>
2024-08-29 05:09:02 -0400

    Refactor `bindPendingParams()` method in `oci/Command.php`,

--- src/db/oci/Command.php
+++ src/db/oci/Command.php
@@ -16,17 +16,25 @@ class Command extends \yii\db\Command
     /**
      * {@inheritdoc}
      */
-    protected function bindPendingParams()
+    protected function bindPendingParams(): void
     {
         $paramsPassedByReference = [];
+
         foreach ($this->pendingParams as $name => $value) {
             if (\PDO::PARAM_STR === $value[1]) {
                 $paramsPassedByReference[$name] = $value[0];
-                $this->pdoStatement->bindParam($name, $paramsPassedByReference[$name], $value[1], strlen($value[0]));
+
+                $this->pdoStatement->bindParam(
+                    $name,
+                    $paramsPassedByReference[$name],
+                    $value[1],
+                    strlen((string) $value[0])
+                );
             } else {
                 $this->pdoStatement->bindValue($name, $value[0], $value[1]);
             }
         }
+
         $this->pendingParams = [];
     }
 
