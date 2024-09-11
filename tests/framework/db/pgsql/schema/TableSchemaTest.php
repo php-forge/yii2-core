b945d8699cc15db177462c1ad7dc8814193e5d97
Wilmer Arambula <terabytesoftw@gmail.com>
2024-09-11 12:29:37 -0300

    Fix tests `pgsql`.

--- tests/framework/db/pgsql/schema/TableSchemaTest.php
+++ tests/framework/db/pgsql/schema/TableSchemaTest.php
@@ -61,7 +61,10 @@ final class TableSchemaTest extends \yiiunit\TestCase
         $this->assertNotNull($tableSchema);
 
         $this->assertEmpty($tableSchema->primaryKey);
-        $this->assertSame('T_autoincrement_id_seq', $tableSchema->sequenceName['id']);
+        $this->assertSame(
+            'T_autoincrement_id_seq',
+            $this->db->getQuoter()->unquoteSimpleColumnName($tableSchema->sequenceName['id'])
+        );
 
         $result = $this->db->createCommand()->dropTable($tableName)->execute();
 
@@ -98,8 +101,14 @@ final class TableSchemaTest extends \yiiunit\TestCase
         $this->assertNotNull($tableSchema);
 
         $this->assertEmpty($tableSchema->primaryKey);
-        $this->assertSame('T_autoincrement_with_multiple_keys_id1_seq', $tableSchema->sequenceName['id1']);
-        $this->assertSame('T_autoincrement_with_multiple_keys_id2_seq', $tableSchema->sequenceName['id2']);
+        $this->assertSame(
+            'T_autoincrement_with_multiple_keys_id1_seq',
+            $this->db->getQuoter()->unquoteSimpleColumnName($tableSchema->sequenceName['id1'])
+        );
+        $this->assertSame(
+            'T_autoincrement_with_multiple_keys_id2_seq',
+            $this->db->getQuoter()->unquoteSimpleColumnName($tableSchema->sequenceName['id2'])
+        );
 
         $result = $this->db->createCommand()->dropTable($tableName)->execute();
 
@@ -135,7 +144,10 @@ final class TableSchemaTest extends \yiiunit\TestCase
         $this->assertNotNull($tableSchema);
 
         $this->assertSame(['id'], $tableSchema->primaryKey);
-        $this->assertSame('T_autoincrement_with_pk_id_seq', $tableSchema->sequenceName['id']);
+        $this->assertSame(
+            'T_autoincrement_with_pk_id_seq',
+            $this->db->getQuoter()->unquoteSimpleColumnName($tableSchema->sequenceName['id'])
+        );
 
         $result = $this->db->createCommand()->dropTable($tableName)->execute();
 
@@ -173,8 +185,14 @@ final class TableSchemaTest extends \yiiunit\TestCase
         $this->assertNotNull($tableSchema);
 
         $this->assertSame(['id1', 'id2'], $tableSchema->primaryKey);
-        $this->assertSame('T_autoincrement_with_pk_composite_id1_seq', $tableSchema->sequenceName['id1']);
-        $this->assertSame('T_autoincrement_with_pk_composite_id2_seq', $tableSchema->sequenceName['id2']);
+        $this->assertSame(
+            'T_autoincrement_with_pk_composite_id1_seq',
+            $this->db->getQuoter()->unquoteSimpleColumnName($tableSchema->sequenceName['id1'])
+        );
+        $this->assertSame(
+            'T_autoincrement_with_pk_composite_id2_seq',
+            $this->db->getQuoter()->unquoteSimpleColumnName($tableSchema->sequenceName['id2'])
+        );
 
         $result = $this->db->createCommand()->dropTable($tableName)->execute();
 
