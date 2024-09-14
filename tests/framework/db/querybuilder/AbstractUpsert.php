3f9605d8eff845f7654c10af398a2e82e4ae20de
Wilmer Arambula <terabytesoftw@gmail.com>
2024-09-14 04:41:34 -0300

    Rework tests.

--- tests/framework/db/querybuilder/AbstractUpsert.php
+++ tests/framework/db/querybuilder/AbstractUpsert.php
@@ -5,10 +5,7 @@ declare(strict_types=1);
 namespace yiiunit\framework\db\querybuilder;
 
 use yii\db\Connection;
-use yii\db\Query;
 use yii\db\QueryInterface;
-
-use function json_encode;
 
 abstract class AbstractUpsert extends \yiiunit\TestCase
 {
@@ -23,7 +20,7 @@ abstract class AbstractUpsert extends \yiiunit\TestCase
     }
 
     public function testUpsert(
-        string $table,
+        string $tableName,
         array|QueryInterface $insertColumns,
         array|bool $updateColumns,
         string $expectedSQL,
@@ -32,66 +29,9 @@ abstract class AbstractUpsert extends \yiiunit\TestCase
         $actualParams = [];
 
         $qb = $this->db->getQueryBuilder();
-        $actualSQL = $qb->upsert($table, $insertColumns, $updateColumns, $actualParams);
+        $actualSQL = $qb->upsert($tableName, $insertColumns, $updateColumns, $actualParams);
 
         $this->assertSame($expectedSQL, $actualSQL);
         $this->assertSame($expectedParams, $actualParams);
-    }
-
-    public function testUpsertExecute(
-        string $table,
-        array|QueryInterface $insertColumns,
-        array|bool $updateColumns
-    ): void {
-        $actualParams = [];
-
-        $qb = $this->db->getQueryBuilder();
-        $actualSQL = $qb->upsert($table, $insertColumns, $updateColumns, $actualParams);
-
-        $countQuery = (new Query())->from($table)->select('count(*)');
-        $rowCountBefore = (int) $countQuery->createCommand($this->db)->queryScalar();
-        $command = $this->db->createCommand($actualSQL, $actualParams);
-
-        $this->assertEquals(1, $command->execute());
-
-        $rowCountAfter = (int) $countQuery->createCommand($this->db)->queryScalar();
-
-        $this->assertEquals(1, $rowCountAfter - $rowCountBefore);
-
-        $command = $this->db->createCommand($actualSQL, $actualParams);
-
-        $command->execute();
-    }
-
-    public function testUpsertVarbinary(): void
-    {
-        $params = [];
-
-        $qb = $this->db->getQueryBuilder();
-
-        $testData = json_encode(['test' => 'string', 'test2' => 'integer'], JSON_THROW_ON_ERROR);
-
-        $result = $this->db->createCommand(
-            $qb->upsert(
-                'T_upsert_varbinary',
-                ['id' => 1, 'blob_col' => $testData],
-                ['blob_col' => $testData],
-                $params,
-            ),
-            $params,
-        )->execute();
-
-        $this->assertSame(1, $result);
-
-        $query = (new Query())->select(['blob_col as blob_col'])->from('T_upsert_varbinary')->where(['id' => 1]);
-        $resultData = $query->createCommand($this->db)->queryOne();
-
-        $this->assertIsArray($resultData);
-
-        if (is_resource($resultData['blob_col'])) {
-            $resultData['blob_col'] = stream_get_contents($resultData['blob_col']);
-        }
-
-        $this->assertSame($testData, $resultData['blob_col']);
     }
 }
