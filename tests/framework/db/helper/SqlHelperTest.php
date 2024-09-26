a8f0a5368774603fa9ebf5190f7d2310607156d5
Wilmer Arambula <terabytesoftw@gmail.com>
2024-09-26 06:43:46 -0300

    Add method `addSuffix()` in `SqlHelper::class`.

--- tests/framework/db/helper/SqlHelperTest.php
+++ tests/framework/db/helper/SqlHelperTest.php
@@ -7,10 +7,31 @@ namespace yiiunit\framework\db\helper;
 use yii\db\SqlHelper;
 
 /**
- * @group sqlhelper
+ * @group sql-helper
  */
 final class SqlHelperTest extends \yiiunit\TestCase
 {
+    public function testAddSuffix(): void
+    {
+        $this->assertSame('example_SEQ', SqlHelper::addSuffix('example', '_SEQ'));
+    }
+
+    public function testAddSuffixWithAlreadyPresent(): void
+    {
+        $result = SqlHelper::addSuffix('example_SEQ', '_SEQ');
+
+        $this->assertSame('example_SEQ', $result);
+
+        $resultCaseInsensitive = SqlHelper::addSuffix('example_seq', '_SEQ');
+
+        $this->assertSame('example_SEQ', $resultCaseInsensitive);
+    }
+
+    public function testAddSuffixWithEmpty(): void
+    {
+        $this->assertEquals('example', SqlHelper::addSuffix('example', ''));
+    }
+
     public function testCleanSql(): void
     {
         $dirtySQL = <<<SQL
